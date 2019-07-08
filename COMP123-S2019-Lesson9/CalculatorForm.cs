49f9be8a4b21df82f82515995375c3c11f912efe
301045522 <301045522@PR-E216-17>
2019-07-08 14:21:23 -0400

    Calculator

--- COMP123-S2019-Lesson9/CalculatorForm.cs
+++ COMP123-S2019-Lesson9/CalculatorForm.cs
@@ -12,9 +12,34 @@ namespace COMP123_S2019_Lesson9
 {
     public partial class CalculatorForm : Form
     {
+        /// <summary>
+        /// This is the constrcot for the calculator form
+        /// </summary>
         public CalculatorForm()
         {
             InitializeComponent();
+        }
+        /// <summary>
+        /// This is the shared event handler for all the calculator buttons
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void CalculatorButton_Click(object sender, EventArgs e)
+        {
+            var TheButton = sender as Button;
+            int buttonvalue;
+            bool resultCondition = int.TryParse(TheButton.Text, out buttonvalue);
+
+            if(resultCondition)
+            {
+                ResultLabel.Text += TheButton.Text;
+            }
+            else
+            {
+                ResultLabel.Text = "Not a Number(NAN)";
+            }
+            ResultLabel.Text = TheButton.Text;
         }
     }
 }
